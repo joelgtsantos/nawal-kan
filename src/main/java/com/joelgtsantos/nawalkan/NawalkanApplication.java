5e8b7378ad05944d8da07f5bc8cffcfbffd664f7
Joel Santos <joelgtsantos@gmail.com>
2021-07-08 20:55:56 -0600

    Add initial base project

---
+++ src/main/java/com/joelgtsantos/nawalkan/NawalkanApplication.java
@@ -0,0 +1,13 @@
+package com.joelgtsantos.nawalkan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NawalkanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NawalkanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joelgtsantos/nawalkan/NawalkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelgtsantos.nawalkan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NawalkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
